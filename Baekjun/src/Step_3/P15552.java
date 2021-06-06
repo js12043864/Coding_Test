f23d276fed6499e7e06237d78c8afab301023ca9
js12043864 <js12043864@naver.com>
2021-06-06 18:11:35 +0900

    step3 code (not completed perfectly)

--- Baekjun/src/Step_3/P15552.java
+++ Baekjun/src/Step_3/P15552.java
@@ -10,22 +10,7 @@ public class P15552 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int cnt = Integer.parseInt(br.readLine());
-		int[] answer = new int[cnt];
-		for(int i = 0; i < cnt; i++) {
-			int a = Integer.parseInt(br.readLine());
-			int b = Integer.parseInt(br.readLine());
-			answer[i] = a + b;
-		}
-		for(int j = 0; j < cnt; j++) {
-			bw.write(answer[j]);
-			bw.newLine();
-		}
-		bw.flush();
-		bw.close();
-		br.close();
+		
 	}
 
 }
