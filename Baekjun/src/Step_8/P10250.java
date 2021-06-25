98f2b6a668b3874de589d56e269f0edb668956d5
js12043864 <js12043864@naver.com>
2021-06-25 17:29:11 +0900

    Step 8

---
+++ Baekjun/src/Step_8/P10250.java
@@ -0,0 +1,23 @@
+package Step_8;
+
+import java.util.Scanner;
+
+public class P10250 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int[] array = new int[a];
+		for(int i = 0; i < a; i++) {
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+			array[i] = 100 * (d % b) + ((d / b) + 1);
+		}
+		for(int j = 0; j < a; j++) {
+			System.out.println(array[j]);
+		}
+	}
+
+}
