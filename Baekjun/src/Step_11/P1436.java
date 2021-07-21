aa3064fa6cbbdaa5cba05148348cffd1a317b97d
js12043864 <js12043864@naver.com>
2021-07-21 22:31:00 +0900

    Step_11/P1436.java

--- Baekjun/src/Step_11/P1436.java
+++ Baekjun/src/Step_11/P1436.java
@@ -1,15 +1,24 @@
 package Step_11;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class P1436 {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int result = 666 + (num-1) * 1000;
-        System.out.println(result);
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int cnt = 1;
+		int num2 = 666;
+		while(cnt != num) {
+			num2++;
+			if(String.valueOf(num2).contains("666")) {
+				cnt++;
+			}
+		}
+		System.out.println(num2);
+	}
 
 }
