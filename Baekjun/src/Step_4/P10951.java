9390a5a1ef5a8c44bc2f35fbd72a3bb3287c6ab2
js12043864 <js12043864@naver.com>
2021-07-30 21:01:55 +0900

    Step_4/P10951.java

--- Baekjun/src/Step_4/P10951.java
+++ Baekjun/src/Step_4/P10951.java
@@ -1,6 +1,5 @@
 package Step_4;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class P10951 {
@@ -8,14 +7,10 @@ public class P10951 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		for(int i = 0; i < 5; i++) {
+		while(sc.hasNext()) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			array.add(a+b);
-		}
-		for(int c : array) {
-			System.out.println(c);
+			System.out.println(a+b);
 		}
 	}
 }
